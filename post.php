552db181e309e89fecd035e92fbe3f4614adc938
okiapi <oatvin@gmail.com>
2017-06-08 14:40:17 +0700

    Create post.php

--- post.php
+++ post.php
@@ -4,8 +4,8 @@ $url = 'https://api.line.me/v2/profile';
 // use key 'http' even if you send the request to https://...
 $options = array(
     'http' => array(
-        'header'  => "Content-type: application/x-www-form-urlencoded",
-        'header'  => "Authorization : Bearer {5OYWAG9qUIO9pG/ML5YiTcvERAkj4D4ApN0+zkdDZ+unN2i7dhcjjhhgGjTNcQLd9vlbR1qYo6XS3ohQ/MnvCn+OnE7y4m/ySchNTMZpBaUQzpz8VQDDPOWIqxOdkrwqqSFxzL7KeHALb6oVq4i7UFyJkyQKxupqlZ5I0qCNTE8=}",
+        'header'  => "Content-type: application/x-www-form-urlencoded\r\n" . 
+                      "Authorization : Bearer {5OYWAG9qUIO9pG/ML5YiTcvERAkj4D4ApN0+zkdDZ+unN2i7dhcjjhhgGjTNcQLd9vlbR1qYo6XS3ohQ/MnvCn+OnE7y4m/ySchNTMZpBaUQzpz8VQDDPOWIqxOdkrwqqSFxzL7KeHALb6oVq4i7UFyJkyQKxupqlZ5I0qCNTE8=}",
         'method'  => 'GET'
     )
 );
