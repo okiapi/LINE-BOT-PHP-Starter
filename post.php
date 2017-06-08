bfa97e12eaa9ea59a28cce566d53458443ad4df2
okiapi <oatvin@gmail.com>
2017-06-08 14:13:32 +0700

    Create post.php

--- post.php
+++ post.php
@@ -1,5 +1,5 @@
 <?
-$url = 'GET https://api.line.me/v2/profile';
+$url = 'https://api.line.me/v2/profile';
 $data = array(
     'Authorization' => 'Bearer {lExPegIfEn52CQaozHfqfX4HJErjh3QSP78ZLzrIkTem7GTlpCzVEpqzlqMiii/79vlbR1qYo6XS3ohQ/MnvCn+OnE7y4m/ySchNTMZpBaVxROCmWAVNlUphWRDaHv5fHDpav9FnldsW9WUJrkqxllyJkyQKxupqlZ5I0qCNTE8=}'
 );
@@ -8,7 +8,7 @@ $data = array(
 $options = array(
     'http' => array(
         'header'  => "Content-type: application/x-www-form-urlencoded",
-        'method'  => 'POST',
+        'method'  => 'GET',
         'content' => http_build_query($data)
     )
 );
