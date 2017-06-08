c26d2d8995088fc9aaca19354caafa47bc9b667d
okiapi <oatvin@gmail.com>
2017-06-08 14:37:40 +0700

    Create post.php

--- post.php
+++ post.php
@@ -1,13 +1,12 @@
 <?
 $url = 'https://api.line.me/v2/profile';
-$data = 'Authorization : Bearer {5OYWAG9qUIO9pG/ML5YiTcvERAkj4D4ApN0+zkdDZ+unN2i7dhcjjhhgGjTNcQLd9vlbR1qYo6XS3ohQ/MnvCn+OnE7y4m/ySchNTMZpBaUQzpz8VQDDPOWIqxOdkrwqqSFxzL7KeHALb6oVq4i7UFyJkyQKxupqlZ5I0qCNTE8=}'
 
 // use key 'http' even if you send the request to https://...
 $options = array(
     'http' => array(
         'header'  => "Content-type: application/x-www-form-urlencoded",
-        'method'  => 'GET',
-        'content' => http_build_query($data)
+        'header'  => "Authorization : Bearer {5OYWAG9qUIO9pG/ML5YiTcvERAkj4D4ApN0+zkdDZ+unN2i7dhcjjhhgGjTNcQLd9vlbR1qYo6XS3ohQ/MnvCn+OnE7y4m/ySchNTMZpBaUQzpz8VQDDPOWIqxOdkrwqqSFxzL7KeHALb6oVq4i7UFyJkyQKxupqlZ5I0qCNTE8=}",
+        'method'  => 'GET'
     )
 );
 $context  = stream_context_create($options);
