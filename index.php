fcaeb9f93045cc86e5a9293662248d72d03c7795
okiapi <oatvin@gmail.com>
2017-06-08 14:03:02 +0700

    Create index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ $data = array(
 // use key 'http' even if you send the request to https://...
 $options = array(
     'http' => array(
-        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        'header'  => "Content-type: application/x-www-form-urlencoded",
         'method'  => 'POST',
         'content' => http_build_query($data)
     )
