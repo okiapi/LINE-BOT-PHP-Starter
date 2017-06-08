4ce244b41b307beac9e00ec47ed23af97ee86a44
okiapi <oatvin@gmail.com>
2017-06-08 13:55:09 +0700

    Create index.php

--- index.php
+++ index.php
@@ -1,2 +1,31 @@
-<?php
-echo "Hello LINE BOT";
+<?
+//API Url
+$url = 'https://api.line.me/v2/oauth/accessToken';
+
+//Initiate cURL.
+$ch = curl_init($url);
+
+//The JSON data.
+$jsonData = array(
+    'grant_type' => 'authorization_code',
+    'code' => 'j0jy3kfwKOKSWrXYs6Cn',
+    'client_id' => '1518868862',
+    'client_secret' => 'e917bfe93dfcaac2ec636d38c1ac9b42',
+    'redirect_uri' => 'http://192.168.11.207/linelogin/line.php'	
+);
+
+//Encode the array into JSON.
+$jsonDataEncoded = json_encode($jsonData);
+
+//Tell cURL that we want to send a POST request.
+curl_setopt($ch, CURLOPT_POST, 1);
+
+//Attach our encoded JSON string to the POST fields.
+curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
+
+//Set the content type to application/json
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json')); 
+
+//Execute the request
+$result = curl_exec($ch);
+?>
