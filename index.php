95ccddf84b05f0bbe5a4bffab25b5f46d2cab3f8
okiapi <oatvin@gmail.com>
2017-06-09 13:46:38 +0700

    Create index.php

--- index.php
+++ index.php
@@ -1,27 +1,26 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
-</head>
-<script>
-$.ajax({ 
-   type: "POST",
-   dataType: "jsonp",
-   data: {
-		grant_type: "authorization_code",
-		client_id: "1518868862",
-		client_secret: "e917bfe93dfcaac2ec636d38c1ac9b42",
-		code: "1Ii0Jts2NFeZKHbx8Yrg",
-		redirect_uri: "https://linebotoki.herokuapp.com/"   
-   },
-   contentType: "application/x-www-form-urlencoded",
-   url: "https://api.line.me/v1/oauth/accessToken",
-   success: function(data){        
-     alert(data);
-   }
-});
-</script>
+<!DOCTYPE html>
+<html>
 <body>
+
+<h1>The XMLHttpRequest Object</h1>
+
+<button type="button" onclick="loadDoc()">Request data</button>
+
+<p id="demo"></p>
+ 
+<script>
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("demo").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("POST", "https://api.line.me/v1/oauth/accessToken", true);
+  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhttp.send("grant_type=authorization_code&client_id=1518868862&client_secret=e917bfe93dfcaac2ec636d38c1ac9b42&code=ySkI9sEYf9gZqewjZsqm&redirect_uri=ttps://linebotoki.herokuapp.com/");
+}
+</script>
+
 </body>
 </html>
