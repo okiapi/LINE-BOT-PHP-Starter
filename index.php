17849fe53a64122715b8596941de04dcb36c6b98
okiapi <oatvin@gmail.com>
2017-06-09 13:05:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,25 +1,27 @@
-<?
-$url = 'https://api.line.me/v2/oauth/accessToken';
-$data = array(
-    'grant_type' => 'authorization_code',
-    'code' => 'mtofEo62mjrP38xjXTCD',
-    'client_id' => '1518868862',
-    'client_secret' => 'e917bfe93dfcaac2ec636d38c1ac9b42',
-    'redirect_uri' => 'https://linebotoki.herokuapp.com/'	
-);
-
-// use key 'http' even if you send the request to https://...
-$options = array(
-    'http' => array(
-        'header'  => "Content-type: application/x-www-form-urlencoded",
-        'method'  => 'POST',
-        'content' => http_build_query($data)
-    )
-);
-$context  = stream_context_create($options);
-$result = file_get_contents($url, false, $context);
-if ($result === FALSE) { /* Handle error */ }
-
-var_dump($result);
-
-?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+<script>
+$.ajax({ 
+   type: "POST",
+   dataType: "jsonp",
+   data: {
+		grant_type: "authorization_code",
+		client_id: "1518868862",
+		client_secret: "e917bfe93dfcaac2ec636d38c1ac9b42",
+		code: "1Ii0Jts2NFeZKHbx8Yrg",
+		redirect_uri: "https://linebotoki.herokuapp.com/"   
+   },
+   contentType: "application/x-www-form-urlencoded",
+   url: "https://api.line.me/v1/oauth/accessToken",
+   success: function(data){        
+     alert(data);
+   }
+});
+</script>
+<body>
+</body>
+</html>
