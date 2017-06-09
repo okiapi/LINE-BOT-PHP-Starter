848226732318b976f133643dcd28856e6cc190ee
okiapi <oatvin@gmail.com>
2017-06-09 16:25:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
  <script>
 function loginLine(){
-    var win = window.open("https://access.line.me/dialog/oauth/weblogin?response_type=code&client_id=1518868862&redirect_uri=http://linebotoki.herokuapp.com/line.php&state=a001",'Popup','height=500,width=480');
+    var win = window.open("https://access.line.me/dialog/oauth/weblogin?response_type=code&client_id=1518868862&redirect_uri=https://linebotoki.herokuapp.com/line.php&state=123",'Popup','height=500,width=480');
     win.window.focus();
 }
 </script>
