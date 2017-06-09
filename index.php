f2c4fc522f6c851ef5da2fda1df726ed7f3675d2
okiapi <oatvin@gmail.com>
2017-06-09 13:56:31 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <body>
 
 <h1>The XMLHttpRequest Object</h1>
@@ -18,7 +19,7 @@ function loadDoc() {
   };
   xhttp.open("POST", "https://api.line.me/v1/oauth/accessToken", true);
   xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-  xhttp.send("grant_type=authorization_code&client_id=1518868862&client_secret=e917bfe93dfcaac2ec636d38c1ac9b42&code=ySkI9sEYf9gZqewjZsqm&redirect_uri=https://linebotoki.herokuapp.com/");
+  xhttp.send("grant_type=authorization_code&client_id=1518868862&client_secret=e917bfe93dfcaac2ec636d38c1ac9b42&code=1Ii0Jts2NFeZKHbx8Yrg&redirect_uri=ttps://linebotoki.herokuapp.com/");
 }
 </script>
 
